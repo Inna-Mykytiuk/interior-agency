1ebe8862c6c678701fea5bb1d12f5ffbe79c061c
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-08-04 20:05:03 +0300

    Revert "create Sanity works page, add content"
    This reverts commit ccd304e108493047c8d5efbcf56a959c41778798.

--- app/(site)/works/page.tsx
+++ app/(site)/works/page.tsx
@@ -1,11 +1,4 @@
 import React from "react";
-export const revalidate = 0;
-
-import { getWorksPage, ImageType, ProjectType } from "@/sanity/queries/page";
-import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
-import Marquee from "react-fast-marquee";
 
 const WorksPage = () => {
   return (
